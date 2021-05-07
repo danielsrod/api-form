00cc276295c71d6060b8aa4aea06bc5d08a560b3
danielsrod <coeng.daniel@gmail.com>
2021-05-07 12:28:49 -0400

    rota post para enviar informações do formulario preenchido com os dados do usuario

--- src/routes/usuariosRouter.js
+++ src/routes/usuariosRouter.js
@@ -7,4 +7,20 @@ router.get('/', allUsers);
 
 router.get('/:id', uniqueUser);
 
+router.post('/:id', (req, res) => {
+    const { id } = req.params;
+    // Enviar para o bd, os dados
+    // do formulario preenchido
+    // No caso enviar a imagem
+    // assinada pelo paciente
+
+    // INSERT INTO usuarios (coluna onde a imagem via ficar armazenada)
+    // WHERE nr_paciente = id - filtrar o usuario
+    // VALUES (base64image)
+
+    console.log(req.body);
+
+    res.send(`Usuario com id ${id} foi cadastrado`);
+})
+
 module.exports = router;
