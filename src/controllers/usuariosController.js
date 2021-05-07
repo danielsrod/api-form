7804a803052a6d64721be8018eda5ad804f650dc
danielsrod <coeng.daniel@gmail.com>
2021-05-07 12:37:32 -0400

    exportado para o controller

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -3,8 +3,8 @@ const connection = require('../models/db');
 const allUsers = (req, res) => {
     connection.query(
         'SELECT * FROM `usuarios`',
-        function(err, results, fields) {
-            if(!results) {
+        function (err, results, fields) {
+            if (!results) {
                 res.send(err)
             } else {
                 res.json(results)
@@ -17,8 +17,8 @@ const uniqueUser = (req, res) => {
     const { id } = req.params;
     connection.query(
         "SELECT * FROM usuarios WHERE nr_paciente = '" + id + "'",
-        function(err, results, fields) {
-            if(!results) {
+        function (err, results, fields) {
+            if (!results) {
                 res.send(err)
             } else {
                 res.json(results)
@@ -27,7 +27,26 @@ const uniqueUser = (req, res) => {
     )
 }
 
+const sendForm = (req, res) => {
+    const { id } = req.params;
+
+
+
+    /*
+     enviar para o bd a imagem
+     do forumario preenchido em
+     base64, inserindo no campo
+     correspondente ao usuario
+     */
+
+    console.log(req.body);
+
+    res.send(`formulario enviado para o banco de dados. id do usuario ${id}`);
+}
+
+
 module.exports = {
     allUsers,
     uniqueUser,
+    sendForm
 };
--- src/routes/usuariosRouter.js
+++ src/routes/usuariosRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const {allUsers, uniqueUser} = require('../controllers/usuariosController');
+const {allUsers, uniqueUser, sendForm} = require('../controllers/usuariosController');
 
 const router = Router();
 
@@ -7,20 +7,6 @@ router.get('/', allUsers);
 
 router.get('/:id', uniqueUser);
 
-router.post('/:id', (req, res) => {
-    const { id } = req.params;
-    // Enviar para o bd, os dados
-    // do formulario preenchido
-    // No caso enviar a imagem
-    // assinada pelo paciente
-
-    // INSERT INTO usuarios (coluna onde a imagem via ficar armazenada)
-    // WHERE nr_paciente = id - filtrar o usuario
-    // VALUES (base64image)
-
-    console.log(req.body);
-
-    res.send(`Usuario com id ${id} foi cadastrado`);
-})
+router.post('/:id', sendForm)
 
 module.exports = router;
