c3af815bfa3d69348bf62de9833e628d9fea1f78
danielsrod <coeng.daniel@gmail.com>
2021-05-07 15:56:34 -0400

    initial commit

--- src/routes/usuariosRouter.js
+++ src/routes/usuariosRouter.js
@@ -4,17 +4,20 @@ const {
     uniqueUser, 
     sendUserData, 
     sendOnlyForm,
-    
+
  } = require('../controllers/usuariosController');
 
 const router = Router();
 
+// Listar todos os usuarios
 router.get('/', allUsers);
 
+// Pegar dados de um usuario
 router.get('/:id', uniqueUser);
 
 router.post('/:id', sendUserData);
 
+// Atualizar campo de formulario
 router.patch('/:id', sendOnlyForm);
 
 module.exports = router;
