72e18c78eb44639c3a324961c56516e142dc3d5a
danielsrod <coeng.daniel@gmail.com>
2021-05-08 12:47:30 -0400

    falta adicionar os valores para conectar ao bd de teste da tasy

--- src/models/db.js
+++ src/models/db.js
@@ -5,36 +5,28 @@ dotenv.config();
 
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
-/* 
-const connextionConfig = {
-    user: process.env.APPV2_USER,
-    password: process.env.APPV2_PASSWD,
-    connectionString: `${process.env.APPV2_SERVER}/${process.env.APPV2_DB}`,
+const connConfig = {
+    user: "",
+    password: "",
+    connectionString: "",
     poolMin: 5,
     poolMax: 30,
     poolPingInterval: 10,
     autoCommit: true,
     poolTimeout: 360,
-    queueTimeout: 2020000
+    queueTimeout: 2020000,
 }
-//oracledb.queueTimeout = 2020000;
-const poolPromiseOracle = oracledb.createPool(connextionConfig)
-  .then(function (pool){
-    console.log('Connected to Oracle')
-    return pool
-  })
-  .catch(function (err){
-    console.log('Oracle connection', err)
-    return 
-  })
 
-  
-
-
+const poolPromiseOracle = oracledb.createPool(connConfig)
+    .then(function (pool) {
+        console.log('Connected to Oracle');
+        return pool;
+    })
+    .catch(function (err) {
+        console.log('Oracle connection', err);
+        return;
+    })
 
 module.exports = {
-  poolPromiseOracle
+    poolPromiseOracle;
 }
-*/
-
-module.exports = connection;
