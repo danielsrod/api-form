703af39ab73dd895cf73d21ccdc038091cb13408
danielsrod <coeng.daniel@gmail.com>
2021-05-08 12:45:15 -0400

    template de conexão com o banco

--- src/models/db.js
+++ src/models/db.js
@@ -5,10 +5,36 @@ dotenv.config();
 
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
-const connection = await oracledb.getConnection({
-    user: "tasy",
-    password: "aloisk",
-    connectionString: "dbteste.sameldm.com",
-});
+/* 
+const connextionConfig = {
+    user: process.env.APPV2_USER,
+    password: process.env.APPV2_PASSWD,
+    connectionString: `${process.env.APPV2_SERVER}/${process.env.APPV2_DB}`,
+    poolMin: 5,
+    poolMax: 30,
+    poolPingInterval: 10,
+    autoCommit: true,
+    poolTimeout: 360,
+    queueTimeout: 2020000
+}
+//oracledb.queueTimeout = 2020000;
+const poolPromiseOracle = oracledb.createPool(connextionConfig)
+  .then(function (pool){
+    console.log('Connected to Oracle')
+    return pool
+  })
+  .catch(function (err){
+    console.log('Oracle connection', err)
+    return 
+  })
+
+  
+
+
+
+module.exports = {
+  poolPromiseOracle
+}
+*/
 
 module.exports = connection;
