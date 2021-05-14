9e55a20bb953717639a3a7c4d0551dfaa7d29f52
danielsrod <coeng.daniel@gmail.com>
2021-05-14 15:47:54 -0400

    . .

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const usuariosRouter = require('./routes/usuariosRouter');
 const termosRouter = require('./routes/termosRouter');
 
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
