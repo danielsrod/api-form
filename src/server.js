46181df54d1ba96d0a7341db207fd3256a770a9e
danielsrod <coeng.daniel@gmail.com>
2021-05-19 10:53:10 -0400

    limit alterado para 50mb
    parece que o default era 1mb, e podia dar problema na hora de mandar uma imagem maior que 1mb

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const usuariosRouter = require('./routes/usuariosRouter');
 const termosRouter = require('./routes/termosRouter');
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.get('/', (req, res) => {
     console.log('home');
