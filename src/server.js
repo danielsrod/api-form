e87ada479227eddff3a7372d17024dde28cecaa6
danielsrod <coeng.daniel@gmail.com>
2021-05-26 16:30:24 -0400

    dotenv-flow, compression e helmet adicionados

--- src/server.js
+++ src/server.js
@@ -5,11 +5,15 @@ const morgan = require('morgan')
 const cors = require('cors');
 const usuariosRouter = require('./routes/usuariosRouter');
 const termosRouter = require('./routes/termosRouter');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 app.use(morgan('dev'));
+app.use(compression());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
